3e456fbd10751e921c665d447447272b55119cce
Shalabh Agarwal <sagarwal10@hotmail.com>
2023-07-21 16:20:33 -0700

    Add method for updating user info

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,5 +46,36 @@ router.get('/user', auth.required, function(req, res, next) {
   }).catch(next);
 }); 
 
+router.put('/user', auth.required, function(req, res, next) {
+  User.findById(req.payload.id).then(function(user) {
+    if (!user) return res.sendStatus(401);
+
+    // only update fields that were actually passed
+    if (typeof req.body.user.username !== 'undefined') {
+      user.username = req.body.user.username;
+    }
+
+    if (typeof req.body.user.email !== 'undefined') {
+      user.email = req.body.user.email;
+    }
+
+    if (typeof req.body.user.bio !== 'undefined') {
+      user.bio = req.body.user.bio;
+    }
+
+    if (typeof req.body.user.image !== 'undefined') {
+      user.image = req.body.user.image;
+    }
+
+    if (typeof req.body.user.password !== 'undefined') {
+      user.setPassword(req.body.user.password);
+    }
+
+    return user.save().then(function() {
+      return res.json({user: user.toAuthJSON()});
+    });
+  });
+}); 
+   
 module.exports = router; 
 
