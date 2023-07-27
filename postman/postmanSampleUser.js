6444f365bb60cd402f39937455b4cd1f2c75309b
Shalabh Agarwal <sagarwal10@hotmail.com>
2023-07-26 18:00:53 -0700

    Add postman API test and user

---
+++ postman/postmanSampleUser.js
@@ -0,0 +1,40 @@
+{
+  "id": "410edddc-dbbf-5d80-9c82-759f9d13096b",
+  "name": "Conduit",
+  "values": [
+    {
+      "enabled": true,
+      "key": "APIURL",
+      "value": "http://localhost:3000/api",
+      "type": "text"
+    },
+    {
+      "enabled": true,
+      "key": "USERNAME",
+      "value": "daffy",
+      "type": "text"
+    },
+    {
+      "enabled": true,
+      "key": "EMAIL",
+      "value": "daffyduck@disney.com",
+      "type": "text"
+    },
+    {
+      "enabled": true,
+      "key": "PASSWORD",
+      "value": "daffy",
+      "type": "text"
+    },
+    {
+      "enabled": true,
+      "key": "token",
+      "value": "",
+      "type": "text"
+    }
+  ],
+  "timestamp": 1489786196110,
+  "_postman_variable_scope": "environment",
+  "_postman_exported_at": "2017-03-17T21:30:01.555Z",
+  "_postman_exported_using": "Postman/4.10.3"
+}
