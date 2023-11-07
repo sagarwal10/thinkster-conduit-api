68b9e6492c05ef7b647d36757a7c3c9ac67f51cd
Shalabh Agarwal <sagarwal10@hotmail.com>
2023-11-06 17:49:28 -0800

    Added api endpoint for article query and feed

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -168,3 +168,97 @@ router.delete('/:article/comments/:comment', auth.required, function(req, res, n
   }
 }); 
 
+router.get('/', auth.optional, function(req, res, next) {
+  var query = {};
+  var limit = 20;
+  var offset = 0;
+
+  if (typeof req.query.limit !== 'undefined') {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== 'undefined') {
+    offset = req.query.offset;
+  }
+
+  if (typeof req.query.tag !== 'undefined') {
+    query.tagList = { "$in" : [req.query.tag] };
+  } 
+
+  Promise.all([
+    req.query.author ? User.findOne({ username: res.query.author }) : null,
+    req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
+  }).then(function(results) {
+    var authpr = results[0];
+    var favoriter = results[1];
+   
+    if (author) {
+      query.author = author._id;
+    }
+
+    if (favoriter) {
+      query._id = { $in: favoriter.favorites };
+    } else {
+      query._id = { $in: [] };
+    }
+
+    return Promise.all([
+      Article.find(query)
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .sort({createdAt: 'desc'})
+        .populate('author')
+        .exec(),
+      Article.count(query).exec(),
+      req.payload ? User.findById(req.payload.id) : null,
+    ]).then(function(results) {
+      var articles = results[0];
+      var articlesCount = results[1];
+      var user = results[2]; 
+
+      return res.json({
+        articles: articles.map(function(article) {
+        		return article.toJSONFor(user);
+                  }),
+        articlesCount: articlesCount
+      });
+    });
+  }).catch(next);
+});
+
+router.get('/feed', auth.required, function(req, res, next) {
+  var limit = 20;
+  var offset = 0;
+
+  if (typeof req.query.limit !== 'undefined') {
+    limit = req.query.limit;
+  }
+
+  if (typeof req.query.offset !== 'undefined') {
+    offset = req.query.offset;
+  }
+
+  User.findById(req.payload.id).then(function(user) {
+    if (!user) { requrn res.sendStatus(401); }
+   
+    Promise.all([
+      Article.find({ author: {$in: user.following} })
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .populate('author')
+        .exec(),
+      Article.count({ author: {$in: user.following} })
+    ]).then(function(results) {
+      var articles = results[0];
+      var articlesCount = resuls[1];
+
+      return res.json({
+        articles: articles.map(function(article) {
+          return article.toJSONFor(user);
+        }),
+        articlesCount: articlesCount
+      });
+    });
+  });
+});
+
